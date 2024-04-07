a3cd585195bac190b7f6ac009f6fb3c91ad8bfd8
Jayneel Kanungo <jayneelkanungo57@gmail.com>
2024-04-07 20:38:04 +0530

    - title update

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Jayneel Kanungo",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: ConstantFonts.poppinsRegular,
